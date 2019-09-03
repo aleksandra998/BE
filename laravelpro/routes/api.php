359644daeb8a2b6e3cd3717e009a52f0535eb019
alee89007 <alee89007@gmail.com>
2019-09-03 10:43:39 +0200

    Add existing file

--- laravelpro/routes/api.php
+++ laravelpro/routes/api.php
@@ -16,3 +16,27 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+
+});
+
+Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@authenticate');
+    Route::get('open', 'DataController@open');
+
+    Route::group(['middleware' => ['jwt.verify']], function() {
+        Route::get('user', 'UserController@getAuthenticatedUser');
+        Route::get('closed', 'DataController@closed');
+    });
